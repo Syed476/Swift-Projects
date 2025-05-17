9269ee0881440cc6ecd7902583e90b281392ed78
Syed Maqsood Ali <smaqsod77@hotmail.com>
2025-05-17 14:46:45 +0100

    updated project

---
+++ Tests/MyFirstProjectTests/MyFirstProjectTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import MyFirstProject
+
+final class MyFirstProjectTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssertEqual("Hello, world!", "Hello, world!")
+    }
+}
